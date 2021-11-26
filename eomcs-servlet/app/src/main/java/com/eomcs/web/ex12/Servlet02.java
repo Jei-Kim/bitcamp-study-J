8f3130017fa6d7cdbaa248a0d1db2d1bfdd0041b
jei-kim <jeje2kim@gmail.com>
2021-11-26 22:22:51 +0900

    .

--- eomcs-servlet/app/src/main/java/com/eomcs/web/ex12/Servlet02.java
+++ eomcs-servlet/app/src/main/java/com/eomcs/web/ex12/Servlet02.java
@@ -1,14 +1,16 @@
-// 서블릿 배치: 1) web.xml 에 배치 정보를 등록
+// 서블릿 배치: 2) 애노테이션으로 배치 정보를 등록
 package com.eomcs.web.ex12;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Servlet01 extends HttpServlet {
+@WebServlet("/ex12/s02")
+public class Servlet02 extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
